73525efcc9712a58e8a736877b60d7dbba24bb02
leommelo <lmmelo096@gmail.com>
2025-04-22 21:22:41 -0300

    integração transação

--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,42 +1,63 @@
+import 'package:bytebank_full/components/centered_message.dart';
 import 'package:bytebank_full/models/transaction.dart';
+import 'package:bytebank_full/webAPI/webclient.dart';
 import 'package:flutter/material.dart';
 
-class TransactionsList extends StatelessWidget {
+class TransactionsList extends StatefulWidget {
+  @override
+  State<TransactionsList> createState() => _TransactionsListState();
+}
+
+class _TransactionsListState extends State<TransactionsList> {
   final List<Transaction> transactions = [];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Transactions'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
+      appBar: AppBar(title: Text('Transactions')),
+      body: FutureBuilder<List<Transaction>>(
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+              final List<Transaction>? transactions = snapshot.data;
+              if (transactions != null && transactions.isNotEmpty) {
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final Transaction transaction = transactions![index];
+                    return Card(
+                      child: ListTile(
+                        leading: Icon(Icons.monetization_on),
+                        title: Text(
+                          transaction.value.toString(),
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text(
+                          transaction.contact.accountNumber.toString(),
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: transactions?.length,
+                );
+              }else {
+                return CenteredMessage(
+                  "No transactions found",
+                  icon: Icons.warning,
+                );
+              }
+
+            default:
+          }
+          return CenteredMessage("unknow error");
         },
-        itemCount: transactions.length,
       ),
     );
   }
 }
-
-
--- lib/webAPI/webclient.dart
+++ lib/webAPI/webclient.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:bytebank_full/models/Contact.dart';
+import 'package:bytebank_full/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_client_with_interceptor.dart';
 import 'package:http_interceptor/interceptor_contract.dart';
@@ -24,8 +28,27 @@ class LoggingInterceptor extends InterceptorContract {
   }
 }
 
+Future<List<Transaction>> findAll() async {
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+  );
+  final Response response = await client.get(
+    Uri.parse("http://192.168.1.5:8080/transactions"),
+  );
 
-void findAll() async {
-  final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-  final Response response = await client.get(Uri.parse("http://192.168.1.5:8080/transactions"));
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = [];
+  for (Map<String, dynamic> transactionJson in decodedJson) {
+    final Map<String, dynamic> contactJson = transactionJson['contact'];
+    final Transaction transacao = Transaction(
+      transactionJson['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ),
+    );
+    transactions.add(transacao);
+  }
+  return transactions;
 }
